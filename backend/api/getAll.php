3976299793052a4b777c26ea8a873e3c70afc554
actupss <julia.schlechtriem@stud.fhgr.ch>
2025-10-13 11:52:49 +0200

    A$nderungen

---
+++ backend/api/getAll.php
@@ -0,0 +1,32 @@
+<?php
+
+// -> daten als json laden
+require_once('../config.php');
+
+// -> json aktivieren
+header('Content-Type: application/json');
+
+// -> verbindung mit der datenbank
+try {
+    // Erstellt eine neue PDO-Instanz mit der Konfiguration aus config.php
+    $pdo = new PDO($dsn, $username, $password, $options);
+
+    // SQL-Query mit Platzhaltern für das Einfügen von Daten
+    $sql = "SELECT * FROM meteoride";
+
+    // Bereitet die SQL-Anweisung vor
+    $stmt = $pdo->prepare($sql);
+
+    // Fügt jedes Element im Array in die Datenbank ein
+    //foreach ($data as $item) {
+        $stmt->execute();
+
+    // -> daten in empfang nehmen
+    $results = $stmt->fetchAll();
+
+    // -> daten als json zurückgeben
+    echo json_encode($results);
+
+}   catch (PDOException $e) {
+    die("Verbindung zur Datenbank konnte nicht hergestellt werden: " . $e->getMessage());
+}
